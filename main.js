90dbfb2dc9334e7775479f9cab84c9a4b29eba25
WanlinZ <130186855+WanlinZ@users.noreply.github.com>
2024-03-30 17:49:31 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -35,6 +35,11 @@ recognition.onresult = function(event) {
 
     document.getElementById("textbox").innerHTML = Content;
     console.log(Content);
+    if(Content =="take my selfie")
+    {
+        console.log("taking selfie ---");
+        speak();
+    }
     speak();
 }
 
@@ -57,3 +62,58 @@ Webcam.set({
 
 });
 camera = document.getElementById("camera");
+
+function take_snapshot()
+{
+Webcam.snap(function(data_uri) {
+document.getElementById("result").innerHTML = '<img id="selfie_imgae" src="'+data_uri+'">';
+});
+}
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function(event) {
+    console.log(event);
+
+    var Content = event.results[0][0].transcript;
+
+    document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+    if(Content =="take my selfie")
+    {
+        console.log("taking selfie ---");
+        speak();
+    }
+
+    function speak(){
+        var synth = window.speechSynthesis;
+
+        speak_data = "Taking your Selfie in 5 seconds";
+
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+        synth.speak(utterThis);
+        Webcam.attach(camera);
+
+        setTimeout(function()
+        {
+            take_snapshot();
+            save();
+        }, 5000);
+
+        function save()
+        {
+link = document.getElementById("link");
+image = document.getElementById("selfieimage").src ;
+link.href = image;
+link.click();
+        }
+    }
+}
